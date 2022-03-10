2dd19917071410539ea2324a6f9dd75640eeb964
MrCheif <63733047+MrCheif@users.noreply.github.com>
2022-03-10 16:15:19 -0700

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Spencer Olson on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
